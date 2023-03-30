ed10de4333c5c81ce4716458f1b395a9b6d66f28
Barashkofff <115090287+Barashkofff@users.noreply.github.com>
2023-03-31 00:03:12 +0300

    Linked building system to first level
    1 Add prefab for second tower
    2 Add prefab for TowerPlace
    3 Updated BuildManager and ShopController scripts
    4 Connected and optimized ShopPanel from the second level

--- Assets/C# Scripts/buildManeger scripts/BuildManager.cs
+++ Assets/C# Scripts/buildManeger scripts/BuildManager.cs
@@ -6,55 +6,14 @@ using UnityEngine.UI;
 
 public class BuildManager : MonoBehaviour
 {
-    public bool building;
     public GameObject shopPanel;
-    public bool road;
-    public bool activeCell;
 
-
-    void Start()
-    {
-        
-    }
-
-
-    void Update()
+    void OnMouseDown()
     {
         if (shopPanel.active)
             return;
 
-        if (Input.GetKeyDown(KeyCode.Mouse0) && transform.GetChild(0).GetComponent<Image>().color == Color.green)
-        {
-            if (road == false)
-            {
-                shopPanel.SetActive(true);
-                activeCell = true;
-            }
-        }
-    }
-
-    void OnMouseEnter()
-    {
-        if (shopPanel.active)
-            return;
-
-        if (building == true)
-            transform.GetChild(0).GetComponent<Image>().color = Color.red;
-        else
-            transform.GetChild(0).GetComponent<Image>().color = Color.green;
-    }
-
-    void OnMouseExit()
-    {
-        transform.GetChild(0).GetComponent<Image>().color = Color.white; 
-    }
-
-
-    public void setBuild(GameObject build)
-    {
-
-        Instantiate(build).transform.position = transform.GetChild(1).transform.position;
-        building = true;
-        activeCell = false;
+        shopPanel.SetActive(true);
+        ShopController.place = gameObject.gameObject;
     }
 }
--- Assets/C# Scripts/buildManeger scripts/ShopController.cs
+++ Assets/C# Scripts/buildManeger scripts/ShopController.cs
@@ -1,50 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopController : MonoBehaviour
 {
-    [Header("Other")]
-    public GameObject shopPanel;
-    public GameObject AllCell;
-
-    [Header("Building")]
-    public GameObject dot;
-    public GameObject dot2;
+    public static GameObject place = null;
 
     void Start()
     {
-        
+        gameObject.SetActive(false);
     }
 
-
-    void Update()
+    public void setBuild(GameObject building)
     {
-        
+
+        Instantiate(building, place.transform.position, place.transform.rotation);
+        Destroy(place);
+        gameObject.SetActive(false);
     }
 
     public void Cancel()
     {
-        shopPanel.SetActive(false);
-        for (int i = 0; i < AllCell.transform.childCount; i++)
-        {
-            if (AllCell.transform.GetChild(i).GetComponent<BuildManager>().activeCell == true)
-            {
-                AllCell.transform.GetChild(i).GetComponent<BuildManager>().activeCell = false;
-                break;
-            }
-        }
-    }
-
-    public void BuildDot()
-    {
-        for (int i = 0; i < AllCell.transform.childCount; i++)
-        {
-            if (AllCell.transform.GetChild(i).GetComponent<BuildManager>().activeCell == true && AllCell.transform.GetChild(i).GetComponent<BuildManager>().building == false)
-            {
-                AllCell.transform.GetChild(i).GetComponent<BuildManager>().setBuild(dot);
-            }
-        }
-        shopPanel.SetActive(false);
+        gameObject.SetActive(false);
     }
 }
